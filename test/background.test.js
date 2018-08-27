5c65bb34fd3e74ed42ecb448048cfe37046defaf
rocksoccer <rocksoccer2004@gmail.com>
2018-08-27 02:00:49 +0100

    Add tests

--- test/background.test.js
+++ test/background.test.js
@@ -1,14 +1,25 @@
-const path = require('path');
-const sinonChai = require('sinon-chai');
-const chai = require('chai');
-const expect = chai.expect;
-chai.use(sinonChai);
+const path = require('path')
+const sinonChai = require('sinon-chai')
+const chai = require('chai')
+const expect = chai.expect
+chai.use(sinonChai)
 
-const webExtensionsJSDOM = require('webextensions-jsdom');
-const manifestPath = path.resolve(path.join(__dirname, 'src/manifest.json'));
+const webExtensionsJSDOM = require('webextensions-jsdom')
+const manifestPath = path.resolve(path.join(__dirname, '../src/manifest.json'))
+let webExtension
 
-describe('hello world', () => {
-    it('just fails', () => {
-        expect(true).to.equal(false);
-    });
-});
+beforeEach(async () => {
+  webExtension = await webExtensionsJSDOM.fromManifest(manifestPath, {wiring: true})
+})
+
+afterEach(async () => {
+  await webExtension.destroy()
+})
+
+it('adds the context menu entry', () => {
+  expect(webExtension.background.browser.menus.create).called
+})
+
+it('adds the menu listener', () => {
+  expect(webExtension.background.browser.menus.onClicked.addListener).called
+})
