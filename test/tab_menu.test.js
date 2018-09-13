59f5ec37e662c1040d32da6d87c6e01ed3d3adbe
rocksoccer <rocksoccer2004@gmail.com>
2018-09-13 22:58:26 +0100

    Finish test for tab menu

--- test/tab_menu.test.js
+++ test/tab_menu.test.js
@@ -16,19 +16,21 @@ describe('tab menu close all', () => {
       },
       expect.any(Function)
     )
-    jest.spyOn(tabUtils, 'closeAllTabs')
+  })
+
+  it('close all tabs when the close all tab menu is clicked', () => {
+    tabMenu.createMenu()
+    jest.spyOn(tabUtils, 'closeAllTabs').mockImplementation()
     browser.listeners.trigger('menus', 'onClicked', [{ menuItemId: 'tab-menu-close-all' }])
     expect(tabUtils.closeAllTabs).toHaveBeenCalled()
   })
 
-  it('close all tabs when the close all tab menu is clicked', () => {
-
+  it('does not close all tabs when other items are clicked in tab menu', () => {
+    tabMenu.createMenu()
+    jest.spyOn(tabUtils, 'closeAllTabs').mockImplementation()
+    browser.listeners.trigger('menus', 'onClicked', [{ menuItemId: 'another-id' }])
+    expect(tabUtils.closeAllTabs).not.toHaveBeenCalled()
   })
-
-  it('does not close all tabs when other items are clicked in tab menu', ()=>{
-
-  });
-  
 
   describe('when there is error when creating menu', () => {
     beforeEach(() => {
@@ -40,7 +42,7 @@ describe('tab menu close all', () => {
     })
 
     it('logs the error to console', () => {
-      jest.spyOn(global.console, 'log')
+      jest.spyOn(global.console, 'log').mockImplementation()
       tabMenu.createMenu()
       expect(global.console.log).toHaveBeenCalledWith(expect.any(String))
     })
